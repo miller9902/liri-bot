77e5a00a03aec4780fb6ca395616e5a6a78c7092
Alex <miller9902@gmail.com>
2018-01-29 18:27:37 -0600

    first

---
+++ key.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'TBwxNnHMGQDC31ZfFXFhV9PyP',
+  consumer_secret: 'HXJNHlXPhFm8MNCoyuYNei76zrMyWQhnZZnENYNcBsrHOEAWCl',
+  access_token_key: '188199039-vH9Qn7Sj0gZvULTO6fzIaP6vs8BgvnyHQaRM45po',
+  access_token_secret: 'CKxagb6BvpsbQ9LDv44dAGB6Ta10qJc5qUZUZbgmRYlRR',
+}
